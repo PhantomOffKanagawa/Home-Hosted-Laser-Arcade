56084157668b473e3ebf21f9df791dbc6a8197d9
Harry <harry@noemail.net>
2017-09-01 19:10:23 -0500

    Both Functional

--- server.js
+++ server.js
@@ -1,42 +1,21 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require("socket.io")(http);
-
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', function(socket) {
-    socket.on('chat message', function(msg) {
-        console.log('message: ' + msg);
-        io.emit('chat message', msg);
-    });
-});
+var request = require("request");
 
 http.listen(3000, function() {
     console.log('listening on *:3000');
 });
 
+var score = 0;
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
 
-
-
-
-
-
-
-
-/*http.createServer(function(req, res) {
-    //res.writeHead(200, { 'Content-Type': 'text/html' });
-    //Get Query
     var query = req.url;
     var team = query.slice(1, 2);
     var ptxt = query.slice(2);
     var points = parseInt(ptxt, 10);
     score += points;
-    //res.write(team);
-    //res.write("<br />");
-    //res.write(score.toString());
-    res.end();
 
     request.put("https://locationPartOfAddress.api.smartthings.com:443/api/smartapps/installations/uuid/switches/toggle", {
             headers: {
@@ -48,6 +27,12 @@ http.listen(3000, function() {
                 return console.error('upload failed:', err);
             }
             console.log('Upload successful!  Server responded with:', body);
-
         })
-}).listen(8080);*/
+});
+
+io.on('connection', function(socket) {
+    socket.on('chat message', function(msg) {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+});
